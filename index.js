421b7501d02cbb73dc3a491d6afa67f3cbc88d83
Szewah Chin <swgrleong@gmail.com>
2018-01-01 14:33:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
   var lowercase = 'hello!'
   if (string === string.toLowerCase()) {
     return 'I can\'t hear you!'
-  } else if (string === string.toUppercase()) {
+  } else if (string === string.toUpperCase()) {
     return 'YES INDEED!'
   } else if (string === 'I love you, Grandma') {
     return 'I love you, too'
