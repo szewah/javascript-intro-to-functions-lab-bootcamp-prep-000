4073714323f986c6446b455669d5413d1159fe91
Szewah Chin <swgrleong@gmail.com>
2018-01-01 14:36:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var uppercase = 'HELLO!'
   var lowercase = 'hello!'
-  if (string === lowercase.toLowerCase()) {
+  if (string === string.toLowerCase()) {
     return 'I can\'t hear you!'
-  } else if (string === uppercase.toUpperCase()) {
+  } else if (string === string.toUpperCase()) {
     return 'YES INDEED!'
   } else if (string === 'I love you, Grandma.') {
     return 'I love you, too.'
