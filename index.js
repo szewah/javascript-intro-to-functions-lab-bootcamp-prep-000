20d3e71e7774a87fe3dd65410438db6e462d1114
Szewah Chin <swgrleong@gmail.com>
2018-01-01 14:26:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
   var lowercase = 'hello!'
   if (string === lowercase) {
     return 'I can't hear you'
-  } else if {
+  } else if (string === uppercase) {
     return ''
   }
 }
