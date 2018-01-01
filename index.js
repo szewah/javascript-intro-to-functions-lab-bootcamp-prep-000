d61969faffda45cf73dfb7baa590d0accdfc7ee4
Szewah Chin <swgrleong@gmail.com>
2018-01-01 14:25:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var uppercase = 'HELLO!'
   var lowercase = 'hello!'
-  if (string === lowercase)
+  if (string === lowercase) {
+    return()
+  }
 }
